a4ccf519a512e42fb0044e3e58c6778919ef1f93
Brian olarte <brayan_andrey23@hotmail.com>
2021-04-12 16:08:45 -0500

    estructura base del proyecto con spring initializr

---
+++ src/test/java/co/com/sofka/cargame/CargameApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.cargame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
